983a088141929d1cef9dc168ab23d28caee225b9
mattkjames7 <mattkjames7@gmail.com>
2020-05-07 16:51:29 +0100

    Added docstring for UH density

--- Arase/PWE/ReadUHDensity.py
+++ Arase/PWE/ReadUHDensity.py
@@ -5,7 +5,25 @@ from ..Tools.CDFEpochToUT import CDFEpochToUT
 def ReadUHDensity(Date):
 	'''
 	Reads density measured using UH frequency.
-	'''
+	
+	Inputs
+	======
+	Date : int
+		Integer date in the format yyyymmdd
+	
+	Returns
+	=======
+	data : numpy.recarray
+		Contains the following fields:
+				'Date' : int32
+				'ut' : float32
+				'Epoch' : int64
+				'Density' : float32
+				'Fuh' : float32
+				'Quality' : int32
+		
+
+	'''	
 
 	#read the CDF file
 	data,meta = _ReadCDF(Date,'hfa',3,'')		
