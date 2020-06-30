3cb221c68a446e11f875ef73464c1cf6ea09f554
mattkjames7 <mattkjames7@gmail.com>
2020-06-30 16:59:06 +0100

    fixed a few bugs

--- Arase/MEPe/__init__.py
+++ Arase/MEPe/__init__.py
@@ -5,4 +5,6 @@ from .ReadOmni import ReadOmni
 from .Read3D import Read3D
 from .GetPitchAngle import GetPitchAngle
 from .CalculatePADs import CalculatePADs
+from .SavePADs import SavePADs
 from .ReadIndex import ReadIndex
+from .ReadPAD import ReadPAD
